ac0a312207459b51e9c56f63401d7f84ef66aa4e
novraburv <78609440+novraburv@users.noreply.github.com>
2024-07-29 17:51:22 +0700

    add menu to load game before it starts, set autosave each iteration

--- main.rb
+++ main.rb
@@ -2,4 +2,24 @@
 
 require_relative 'lib/game'
 
-Game.new.start
+def handle_answer
+  response = gets.chomp.upcase
+
+  if response == 'Y'
+    Game.load_game('data/save_game.marshal')
+  elsif response == 'N'
+    Game.new.start
+  else
+    handle_answer
+  end
+end
+
+if File.exist? 'data/save_game'
+  puts <<-HEREDOC
+    Save file from previous game is found.
+    Would you like to load it? y/n
+  HEREDOC
+  handle_answer
+else
+  Game.new.start
+end
